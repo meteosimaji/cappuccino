7c8bbc10a61868f60492af9ee342ea100f599f2a
めてお <kawada0912@outlook.jp>
2025-07-19 12:33:05 +0900

    Add thread support for GPT replies

--- discordbot/thread_store.py
+++ discordbot/thread_store.py
@@ -9,3 +9,15 @@ def get(channel_id: int) -> str | None:
 
 def save(channel_id: int, thread_id: str):
     _DB[str(channel_id)] = thread_id
+
+
+def delete(channel_id: int) -> None:
+    """Remove mapping for the given channel if present."""
+    key = str(channel_id)
+    if key in _DB:
+        del _DB[key]
+
+
+def all_items() -> dict[str, str]:
+    """Return a copy of all channel to thread mappings."""
+    return dict(_DB)
